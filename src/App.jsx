6b4db1a58275182fda290765c53cdd035507adf6
Ali Hamza <hamza120025@gmail.com>
2024-02-25 16:53:20 +0000

    added tailwind class sorter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <h1 className="text-3xl text-center p-4 font-bold text-teal-600">
+    <h1 className=" p-4 text-center text-3xl font-bold text-teal-600 ">
       Hello React + Vite + TailwindCSS
     </h1>
   );
