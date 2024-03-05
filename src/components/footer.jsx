ab522d52a22b15c7ee5af537bab33e9f1922fbac
Ali Hamza <hamza120025@gmail.com>
2024-03-05 21:02:31 +0000

    Add footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+// create a basic footer component
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-[#1F2937] p-2 text-center text-white">
+      <p>&copy; x23257164</p>
+    </div>
+  );
+}
