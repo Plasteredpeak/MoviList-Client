1966d07142e225e54646fadfc1c1985f6b02ddcb
Ali Hamza <hamza120025@gmail.com>
2024-03-05 17:05:09 +0000

    Add header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Router from "./components/Router";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Router />
     </BrowserRouter>
   );
