e513b4a44a56bef4fc4865ef572b5b1fa0f2c49c
Ali Hamza <hamza120025@gmail.com>
2024-04-18 11:32:54 +0100

    Add ToastContainer component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Router from "./components/Router";
 import Header from "./components/Header";
 import Footer from "./components/footer";
@@ -12,6 +15,16 @@ export default function App() {
       <Header />
       <Router />
       <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="light"
+      />
     </BrowserRouter>
   );
 }
