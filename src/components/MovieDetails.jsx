fb1a2539a5b6e02a4f1288fbca9b22e6551eda92
Ali Hamza <hamza120025@gmail.com>
2024-04-18 13:13:41 +0100

    Add movie action icons and tooltips in MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { getMovie, getSeries } from "../services/tmdb.services";
+import { FaArrowDown, FaCalendar, FaCheck, FaPlay } from "react-icons/fa6";
 
 export default function MovieDetails({ isMovie, id }) {
   const [movie, setMovie] = useState(null);
@@ -24,13 +25,64 @@ export default function MovieDetails({ isMovie, id }) {
   }
 
   return (
-    <div className="container mx-auto min-h-[90vh] px-20">
+    <div className="container mx-auto px-20">
       <div className="mt-8 flex">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
-          alt={movie?.title}
-          className="h-96 w-64 rounded-md"
-        />
+        <div className="flex-shrink-0">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
+            alt={movie?.title}
+            className="h-96 w-64 rounded-md"
+          />
+          {/* <div className="dropdown-top dropdown dropdown-hover mt-2 w-full">
+            <div tabIndex={0} role="button" className="btn btn-primary w-full">
+              Add to List
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-full p-2 shadow"
+            >
+   
+              <li>
+                <a href="#" className="flex items-center gap-2">
+                  <FaCheck />
+                  Completed
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-2">
+                  <FaCalendar />
+                  Planning
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-2">
+                  <FaPlay />
+                  Watching
+                </a>
+              </li>
+            </ul>
+          </div> */}
+
+          <div className="mt-2 flex w-full justify-center">
+            <div className="tooltip tooltip-bottom" data-tip="Add to Completed">
+              <div className="text-md btn btn-circle btn-secondary btn-outline group mx-2">
+                <FaCheck className="text-lg group-hover:text-white" />
+              </div>
+            </div>
+
+            <div className="tooltip tooltip-bottom" data-tip="Add to Planning">
+              <div className="text-md btn btn-circle btn-secondary btn-outline group">
+                <FaCalendar className="text-lg group-hover:text-white" />
+              </div>
+            </div>
+
+            <div className="tooltip tooltip-bottom" data-tip="Add to Watching">
+              <div className="text-md btn btn-circle btn-secondary btn-outline group mx-2">
+                <FaPlay className="text-lg group-hover:text-white" />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="ml-8">
           <h1 className="text-4xl font-bold">{movie.title || movie.name}</h1>
           <div className="mt-4">
