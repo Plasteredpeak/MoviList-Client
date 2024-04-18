00357a4519baac2e19060627a4dcbe07cc908918
Ali Hamza <hamza120025@gmail.com>
2024-04-18 11:33:38 +0100

    Update Home component to include loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,11 @@ export default function Home() {
   const [topRated, setTopRated] = useState([]);
   const [series, setSeries] = useState([]);
   const [topRatedSeries, setTopRatedSeries] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true);
       const popularMovies = await getMovies();
       const trending = await getTrending();
       const topRatedMovies = await getTopRatedMovies();
@@ -29,17 +31,26 @@ export default function Home() {
       setSeries(popularSeries.results);
       setTopRated(topRatedMovies.results);
       setTopRatedSeries(topRatedSeries.results);
+      setLoading(false);
     };
     fetchMovies();
   }, []);
 
   return (
-    <div className="mx-auto mb-5">
-      <ListComponent title="Trending" movies={trending} />
-      <ListComponent title="Popular Movies" movies={popular} />
-      <ListComponent title="Popular Series" movies={series} />
-      <ListComponent title="Top Rated Movies" movies={topRated} />
-      <ListComponent title="Top Rated Series" movies={topRatedSeries} />
+    <div className="min-h[90vh] mx-auto mb-5">
+      {loading ? (
+        <div className="flex h-screen items-center justify-center">
+          <span className="loading loading-dots loading-lg"></span>
+        </div>
+      ) : (
+        <>
+          <ListComponent title="Trending" movies={trending} />
+          <ListComponent title="Popular Movies" movies={popular} />
+          <ListComponent title="Popular Series" movies={series} />
+          <ListComponent title="Top Rated Movies" movies={topRated} />
+          <ListComponent title="Top Rated Series" movies={topRatedSeries} />
+        </>
+      )}
     </div>
   );
 }
